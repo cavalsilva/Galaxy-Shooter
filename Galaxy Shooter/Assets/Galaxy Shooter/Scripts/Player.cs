67697807592f6b6de1b97a95420e0f935107acd0
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2018-02-27 23:52:48 -0300

    Player
    Player movement, wrapping, bounds.

--- Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float speed = 5.0f;
+    
+    // Use this for initialization
+	void Start ()
+    {
         transform.position = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        //Player bounds
+        if (transform.position.y > 4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        //Wrapping
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+    }
 }
